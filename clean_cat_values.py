174f8f5d5423501cc900197c7544b30e6823c616
Brad Johnson <climatebrad@gmail.com>
2019-12-17 15:10:43 -0500

    fill_na values for categories

--- clean_cat_values.py
+++ clean_cat_values.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 CLEAN_CAT_VALUES = {
-    'inout' : { 'S' : np.NaN },
-    
+    'inout' : { 'S' : np.NaN }, # inside or outside
+}
+
+CAT_FILL_NA_VALUES = {
+    'trhsloc' :  'P', #transit, housing authority -> P = neither
+    'typeofid' : 'O', # type of id from stopped person -> O = other
+    'sector' : 'Z', # should be calculated from pct, fill with dummy value for now
 }
